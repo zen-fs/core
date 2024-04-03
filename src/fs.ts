6794202e98cf13d19494e6af34780c804d00d4a4
James P <jp@drvortex.dev>
2024-04-03 11:21:47 -0500

    Upgraded core Overhauled RPC types and added functions Added file.ts for PortFile

--- src/fs.ts
+++ src/fs.ts
@@ -1,93 +1,14 @@
 import { Cred } from '@zenfs/core/cred.js';
 import { File } from '@zenfs/core/file.js';
 import { Async, FileSystem, type FileSystemMetadata } from '@zenfs/core/filesystem.js';
-import { Stats, type FileType } from '@zenfs/core/stats.js';
+import { Stats } from '@zenfs/core/stats.js';
 import * as RPC from './rpc.js';
-import { ApiError, ErrorCode } from '@zenfs/core';
 
-export interface PortFSOptions {
+export interface PortFSOptions extends Partial<RPC.RequestOptions> {
 	/**
 	 * The target port that you want to connect to, or the current port if in a port context.
 	 */
 	port: RPC.Port;
-
-	/**
-	 * How long to wait for a message to resolve
-	 */
-	timeout?: number;
-}
-
-export class PortFile extends File {
-	constructor(
-		public readonly fs: PortFS,
-		public readonly fd: number,
-		public readonly path: string,
-		public position?: number
-	) {
-		super();
-	}
-
-	stat(): Promise<Stats> {
-		return this.fs.fileRPC(this.fd, 'stat');
-	}
-	statSync(): Stats {
-		throw new ApiError(ErrorCode.ENOTSUP);
-	}
-	truncate(len: number): Promise<void> {
-		return this.fs.fileRPC(this.fd, 'truncate', len);
-	}
-	truncateSync(): void {
-		throw new ApiError(ErrorCode.ENOTSUP);
-	}
-	write(buffer: Uint8Array, offset?: number, length?: number, position?: number): Promise<number> {
-		return this.fs.fileRPC(this.fd, 'write', buffer, offset, length, position);
-	}
-	writeSync(): number {
-		throw new ApiError(ErrorCode.ENOTSUP);
-	}
-	read<TBuffer extends Uint8Array>(buffer: TBuffer, offset?: number, length?: number, position?: number): Promise<{ bytesRead: number; buffer: TBuffer }> {
-		return <Promise<{ bytesRead: number; buffer: TBuffer }>>this.fs.fileRPC(this.fd, 'read', buffer, offset, length, position);
-	}
-	readSync(): number {
-		throw new ApiError(ErrorCode.ENOTSUP);
-	}
-	chown(uid: number, gid: number): Promise<void> {
-		return this.fs.fileRPC(this.fd, 'chown', uid, gid);
-	}
-	chownSync(): void {
-		throw new ApiError(ErrorCode.ENOTSUP);
-	}
-	chmod(mode: number): Promise<void> {
-		return this.fs.fileRPC(this.fd, 'chmod', mode);
-	}
-	chmodSync(): void {
-		throw new ApiError(ErrorCode.ENOTSUP);
-	}
-	utimes(atime: Date, mtime: Date): Promise<void> {
-		return this.fs.fileRPC(this.fd, 'utimes', atime, mtime);
-	}
-	utimesSync(): void {
-		throw new ApiError(ErrorCode.ENOTSUP);
-	}
-	_setType(type: FileType): Promise<void> {
-		return this.fs.fileRPC(this.fd, '_setType', type);
-	}
-	_setTypeSync(): void {
-		throw new ApiError(ErrorCode.ENOTSUP);
-	}
-
-	close(): Promise<void> {
-		return this.fs.fileRPC(this.fd, 'close');
-	}
-	closeSync(): void {
-		throw new ApiError(ErrorCode.ENOTSUP);
-	}
-	sync(): Promise<void> {
-		return this.fs.fileRPC(this.fd, 'sync');
-	}
-	syncSync(): void {
-		throw new ApiError(ErrorCode.ENOTSUP);
-	}
 }
 
 /**
@@ -97,47 +18,19 @@ export class PortFile extends File {
  * of the configuration option of the remote FS.
  */
 export class PortFS extends Async(FileSystem) {
-	protected _port: RPC.Port;
-	protected _currentID: number = 0;
-	protected _requests: Map<number, RPC.RequestPromise> = new Map();
-	protected _timeout: number = 1000;
-
-	protected handleMessage(message: MessageEvent<RPC.Response> | RPC.Response): void {
-		const data: RPC.Response = 'data' in message ? message.data : message;
-		if (!RPC.isMessage(data)) {
-			return;
-		}
-		const { id, value, method, error, stack } = data;
-		const { resolve, reject } = this._requests.get(id);
-		if (error) {
-			const e = <ApiError>(<unknown>value);
-			e.stack += stack;
-			reject(e);
-			this._requests.delete(id);
-			return;
-		}
-		if (method == 'openFile' || method == 'createFile') {
-			const file = new PortFile(this, (<RPC.File>(<unknown>value)).fd, value.path, value.position);
-			resolve(file);
-			this._requests.delete(id);
-			return;
-		}
-
-		resolve(value);
-		this._requests.delete(id);
-		return;
-	}
+	public readonly port: RPC.Port;
+	public readonly options: Partial<RPC.RequestOptions>;
 
 	/**
 	 * Constructs a new PortFS instance that connects with ZenFS running on
 	 * the specified port.
 	 */
-	public constructor({ port, timeout = 1000 }: PortFSOptions) {
+	public constructor({ port, ...options }: PortFSOptions) {
 		super();
-		this._port = port;
-		this._timeout = timeout;
-		port['on' in port ? 'on' : 'addEventListener']('message', (msg: RPC.Response) => {
-			this.handleMessage(msg);
+		this.port = port;
+		this.options = options;
+		port['on' in port ? 'on' : 'addEventListener']('message', (message: RPC.Response) => {
+			RPC.handleResponse(message, this);
 		});
 	}
 
@@ -149,76 +42,61 @@ export class PortFS extends Async(FileSystem) {
 		};
 	}
 
-	public async fileRPC<const T extends RPC.FileMethod>(fd: number, method: T, ...args: RPC.FileArgs<T>): RPC.FileValue<T> {
-		return new Promise((resolve, reject) => {
-			const id = this._currentID++;
-			this._requests.set(id, { resolve, reject });
-			this._port.postMessage({
-				_zenfs: true,
-				scope: 'file',
-				id,
-				fd,
-				method,
-				stack: new Error().stack.slice('Error:'.length),
-				args,
-			});
+	public fileRPC<const T extends RPC.FileMethod>(fd: number, method: T, ...args: RPC.FileArgs<T>): Promise<RPC.FileValue<T>> {
+		return RPC.request<RPC.FileRequest<T>, RPC.FileValue<T>>(this.port, {
+			_zenfs: true,
+			scope: 'file',
+			fd,
+			method,
+			args,
 		});
 	}
 
-	protected async _rpc<const T extends RPC.FSMethod>(method: T, ...args: RPC.FSArgs<T>): RPC.FSValue<T> {
-		return new Promise((resolve, reject) => {
-			const id = this._currentID++;
-			this._requests.set(id, { resolve, reject });
-			this._port.postMessage({
-				_zenfs: true,
-				scope: 'fs',
-				id,
-				method,
-				stack: new Error().stack.slice('Error:'.length),
-				args,
-			});
-			setTimeout(() => {
-				reject(new ApiError(ErrorCode.EIO, 'RPC Failed'));
-			}, this._timeout);
+	protected rpc<const T extends RPC.FSMethod>(method: T, ...args: RPC.FSArgs<T>): Promise<RPC.FSValue<T>> {
+		return RPC.request<RPC.FSRequest<T>, RPC.FSValue<T>>(this.port, {
+			_zenfs: true,
+			scope: 'fs',
+			method,
+			args,
 		});
 	}
 
 	public async ready(): Promise<this> {
-		await this._rpc('ready');
+		await this.rpc('ready');
 		return this;
 	}
 
 	public rename(oldPath: string, newPath: string, cred: Cred): Promise<void> {
-		return this._rpc('rename', oldPath, newPath, cred);
+		return this.rpc('rename', oldPath, newPath, cred);
 	}
 	public async stat(p: string, cred: Cred): Promise<Stats> {
-		return new Stats(await this._rpc('stat', p, cred));
+		return new Stats(await this.rpc('stat', p, cred));
 	}
 	public sync(path: string, data: Uint8Array, stats: Readonly<Stats>): Promise<void> {
-		return this._rpc('sync', path, data, stats);
+		return this.rpc('sync', path, data, stats);
 	}
 	public openFile(p: string, flag: string, cred: Cred): Promise<File> {
-		return this._rpc('openFile', p, flag, cred);
+		return this.rpc('openFile', p, flag, cred);
 	}
 	public createFile(p: string, flag: string, mode: number, cred: Cred): Promise<File> {
-		return this._rpc('createFile', p, flag, mode, cred);
+		return this.rpc('createFile', p, flag, mode, cred);
 	}
 	public unlink(p: string, cred: Cred): Promise<void> {
-		return this._rpc('unlink', p, cred);
+		return this.rpc('unlink', p, cred);
 	}
 	public rmdir(p: string, cred: Cred): Promise<void> {
-		return this._rpc('rmdir', p, cred);
+		return this.rpc('rmdir', p, cred);
 	}
 	public mkdir(p: string, mode: number, cred: Cred): Promise<void> {
-		return this._rpc('mkdir', p, mode, cred);
+		return this.rpc('mkdir', p, mode, cred);
 	}
 	public readdir(p: string, cred: Cred): Promise<string[]> {
-		return this._rpc('readdir', p, cred);
+		return this.rpc('readdir', p, cred);
 	}
 	public exists(p: string, cred: Cred): Promise<boolean> {
-		return this._rpc('exists', p, cred);
+		return this.rpc('exists', p, cred);
 	}
 	public link(srcpath: string, dstpath: string, cred: Cred): Promise<void> {
-		return this._rpc('link', srcpath, dstpath, cred);
+		return this.rpc('link', srcpath, dstpath, cred);
 	}
 }
--- src/rpc.ts
+++ src/rpc.ts
@@ -1,14 +1,27 @@
-import type { File } from '@zenfs/core';
+import { ApiError, ErrorCode, type File } from '@zenfs/core';
 import type { FileSystem, FileSystemMetadata } from '@zenfs/core/filesystem.js';
 import type { TransferListItem } from 'worker_threads';
+import { type PortFS } from './fs.js';
+import { PortFile } from './file.js';
 
 /**
  * An RPC message
  */
-export interface Message {
+export interface Message<TScope extends string, TMethod extends string> {
 	_zenfs: true;
+	scope: TScope;
 	id: number;
-	scope: string;
+	method: TMethod;
+	stack: string;
+}
+
+export interface BaseRequest<TScope extends string, TMethod extends string, TArgs extends unknown[]> extends Message<TScope, TMethod> {
+	args: TArgs;
+}
+
+export interface BaseResponse<TScope extends string, TMethod extends string, TValue> extends Message<TScope, TMethod> {
+	error: boolean;
+	value: Awaited<TValue>;
 }
 
 export interface Port {
@@ -21,41 +34,21 @@ export interface Port {
 /**
  * Extracts an object of properties assignable to P from an object T
  */
-type ExtractProperties<T, P> = {
+export type ExtractProperties<T, P> = {
 	[K in keyof T as T[K] extends infer Prop ? (Prop extends P ? K : never) : never]: T[K];
 };
+
+// Remote file system
 
 type FSAsyncMethods = ExtractProperties<FileSystem, (...args: unknown[]) => Promise<unknown> | FileSystemMetadata>;
 
 export type FSMethod = keyof FSAsyncMethods;
+export type FSArgs<TMethod extends FSMethod = FSMethod> = Parameters<FSAsyncMethods[TMethod]>;
+export type FSRequest<TMethod extends FSMethod = FSMethod> = BaseRequest<'fs', TMethod, FSArgs<TMethod>>;
+export type FSValue<TMethod extends FSMethod = FSMethod> = Awaited<ReturnType<FSAsyncMethods[TMethod]>>;
+export type FSResponse<TMethod extends FSMethod = FSMethod> = BaseResponse<'fs', TMethod, FSValue<TMethod> extends File ? FileData : FSValue<TMethod>>;
 
-type FSRequests = {
-	[M in FSMethod]: {
-		_zenfs: true;
-		scope: 'fs';
-		id: number;
-		method: M;
-		stack: string;
-		args: Parameters<FSAsyncMethods[M]>;
-	};
-};
-
-type FSResponses = {
-	[M in FSMethod]: {
-		_zenfs: true;
-		scope: 'fs';
-		id: number;
-		error: boolean;
-		method: M;
-		stack: string;
-		value: Awaited<ReturnType<FSAsyncMethods[M]>>;
-	};
-};
-
-export type FSRequest<T extends FSMethod = FSMethod> = FSRequests[T];
-export type FSArgs<T extends FSMethod = FSMethod> = FSRequests[T]['args'];
-export type FSResponse<T extends FSMethod = FSMethod> = FSResponses[T]['value'] extends File ? Omit<FSResponses[T], 'value'> & { fd: number } : FSResponses[T];
-export type FSValue<T extends FSMethod = FSMethod> = Promise<FSResponses[T]['value']>;
+// Remote file
 
 export interface FileData {
 	fd: number;
@@ -63,54 +56,89 @@ export interface FileData {
 	position: number;
 }
 
+function isFileData(value: unknown): value is FileData {
+	return typeof value == 'object' && 'fd' in value && 'path' in value && 'position' in value;
+}
+
 export { FileData as File };
 
 type FileAsyncMethods = ExtractProperties<File, (...args: unknown[]) => Promise<unknown>>;
 
 export type FileMethod = keyof FileAsyncMethods;
+export type FileArgs<TMethod extends FileMethod = FileMethod> = Parameters<FileAsyncMethods[TMethod]>;
+export interface FileRequest<TMethod extends FileMethod = FileMethod> extends BaseRequest<'file', TMethod, FileArgs<TMethod>> {
+	fd: number;
+}
+export type FileValue<TMethod extends FileMethod = FileMethod> = Awaited<ReturnType<FileAsyncMethods[TMethod]>>;
+export interface FileResponse<TMethod extends FileMethod = FileMethod> extends BaseResponse<'file', TMethod, FileValue<TMethod>> {
+	fd: number;
+}
 
-type FileRequests = {
-	[M in FileMethod]: {
-		_zenfs: true;
-		scope: 'file';
-		id: number;
-		fd: number;
-		method: M;
-		stack: string;
-		args: Parameters<FileAsyncMethods[M]>;
-	};
-};
-
-type FileResponses = {
-	[M in FileMethod]: {
-		_zenfs: true;
-		scope: 'file';
-		id: number;
-		fd: number;
-		error: boolean;
-		method: M;
-		stack: string;
-		value: Awaited<ReturnType<FileAsyncMethods[M]>>;
-	};
-};
-
-export type FileRequest<T extends FileMethod = FileMethod> = FileRequests[T];
-export type FileArgs<T extends FileMethod = FileMethod> = FileRequests[T]['args'];
-export type FileResponse<T extends FileMethod = FileMethod> = FileResponses[T];
-export type FileValue<T extends FileMethod = FileMethod> = Promise<FileResponses[T]['value']>;
+// general types
 
 export type Request = FSRequest | FileRequest;
-export type Args = FSRequest['args'] | FileRequest['args'];
+export type Args = FSArgs | FileArgs;
 export type Response = FSResponse | FileResponse;
-export type Value = Promise<FSResponse['value']> | Promise<FileResponse['value']>;
+export type Value = FSValue | FileValue;
 
-export function isMessage(arg: unknown): arg is Message {
+export function isMessage(arg: unknown): arg is Message<string, string> {
 	return typeof arg == 'object' && '_zenfs' in arg && !!arg._zenfs;
 }
 
-type Executor = Parameters<ConstructorParameters<typeof Promise>[0]>;
+type _Executor = Parameters<ConstructorParameters<typeof Promise>[0]>;
 
-export interface RequestPromise {
-	resolve: Executor[0];
-	reject: Executor[1];
+export interface Executor {
+	resolve: _Executor[0];
+	reject: _Executor[1];
+}
+
+const executors: Map<number, Executor> = new Map();
+
+let next = 0;
+
+export interface RequestOptions {
+	timeout: number;
+}
+
+export function request<const TRequest extends BaseRequest<string, string, unknown[]>, TValue>(port: Port, request: Omit<TRequest, 'id' | 'stack'>, { timeout = 1000 }: Partial<RequestOptions> = {}): Promise<TValue> {
+	return new Promise<TValue>((resolve, reject) => {
+		const id = next++;
+		executors.set(id, { resolve, reject });
+		port.postMessage({
+			...request,
+			id,
+			stack:  new Error().stack.slice('Error:'.length),
+		});
+		setTimeout(() => {
+			reject(new ApiError(ErrorCode.EIO, 'RPC Failed'));
+		}, timeout);
+	});
+}
+
+export function handleResponse<const TResponse extends BaseResponse<string, string, unknown>>(response: MessageEvent<TResponse> | TResponse, fs?: PortFS): TResponse {
+	const data: TResponse = 'data' in response ? response.data : response;
+	if (!isMessage(data)) {
+		return;
+	}
+	const { id, value, error, stack } = data;
+	const { resolve, reject } = executors.get(id);
+	if (error) {
+		const e = <ApiError>(<unknown>value);
+		e.stack += stack;
+		reject(e);
+		executors.delete(id);
+		return data;
+	}
+
+	if (isFileData(value)) {
+		const { fd, path, position } = <FileData>(<unknown>value);
+		const file = new PortFile(fs, fd, path, position);
+		resolve(file);
+		executors.delete(id);
+		return data;
+	}
+
+	resolve(value);
+	executors.delete(id);
+	return data;
 }
