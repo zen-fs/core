e0824161f236bd5e7fecdbec8c314a3ae185033d
James P <jp@drvortex.dev>
2024-04-12 13:04:14 -0500

    Added promises export Added notes to assignment "test"

--- tests/assignment.ts
+++ tests/assignment.ts
@@ -3,13 +3,18 @@
 /*
 	This test assigns the exported fs module from ZenFS with the one exported from Node.
 	This ensures anything new that is added will be caught
+
+	Notes on omissions and exclusions:
+	- __promisify__ is omitted as it is metadata
+	- native is omitted as zenfs isn't native
+	- ReadStream and WriteStream are excluded since they are polfilled from another module
 */
 
 import { fs as zen } from '../src/index.js';
 import * as node from 'fs';
 
 type Mock = {
-	[K in keyof typeof node]: Omit<(typeof node)[K], '__promisify__' | 'native'>;
+	[K in Exclude<keyof typeof node, 'ReadStream' | 'WriteStream'>]: Omit<(typeof node)[K], '__promisify__' | 'native'>;
 };
 
 const _module: Mock = zen;
