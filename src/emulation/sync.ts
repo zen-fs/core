0c0482502e2e9e8caa6de077f0e4916273db7bd8
James P <jp@drvortex.dev>
2024-04-22 15:35:01 -0500

    Fixed doc comment

--- src/emulation/sync.ts
+++ src/emulation/sync.ts
@@ -885,7 +885,7 @@ export function cpSync(source: PathLike, destination: PathLike, opts?: Node.Copy
 cpSync satisfies typeof Node.cpSync;
 
 /**
- * Synchronous statfs(2). Returns information about the mounted file system which contains path. The callback gets two arguments (err, stats) where stats is an <fs.StatFs> object.
+ * Synchronous statfs(2). Returns information about the mounted file system which contains path.
  * In case of an error, the err.code will be one of Common System Errors.
  * @param path A path to an existing file or directory on the file system to be queried.
  * @param callback
