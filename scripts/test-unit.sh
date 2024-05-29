4166c7a38c649aa2a46dcb9d480c0e9a1a947125
Dr. Vortex <jp@drvortex.dev>
2024-05-29 10:42:03 -0500

    Inlined unit tests script

--- scripts/test-unit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -d dist ]; then
-	>&2 echo -e '\e[1;31mError\e[0m: You must do \e[0;36mnpm run build\e[0m before running unit tests'
-else
-	cross-env NODE_OPTIONS=--experimental-vm-modules npx jest
-fi
