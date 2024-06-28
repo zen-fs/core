86b7c6788bd1ff83b4ee0038b59df15dbe70e9d3
James P. <jp@drvortex.dev>
2024-06-28 12:35:42 -0500

    Folded `Mutex` into `LockedFS`

--- src/backends/locked.ts
+++ src/backends/locked.ts
@@ -1,9 +1,12 @@
-import { ErrnoError } from '../error.js';
+import { Errno, ErrnoError } from '../error.js';
 import type { Cred } from '../cred.js';
 import type { File } from '../file.js';
 import type { FileSystem, FileSystemMetadata } from '../filesystem.js';
-import { Mutex } from '../mutex.js';
 import type { Stats } from '../stats.js';
+
+export interface MutexLock extends PromiseWithResolvers<void> {
+	[Symbol.dispose](): void;
+}
 
 /**
  * This class serializes access to an underlying async filesystem.
@@ -16,9 +19,72 @@ import type { Stats } from '../stats.js';
  * @internal
  */
 export class LockedFS<FS extends FileSystem> implements FileSystem {
-	private mutex: Mutex = new Mutex();
-
 	constructor(public readonly fs: FS) {}
+
+	/**
+	 * The current locks
+	 */
+	private locks: Map<string, MutexLock> = new Map();
+
+	private addLock(path: string): MutexLock {
+		const lock: MutexLock = {
+			...Promise.withResolvers(),
+			[Symbol.dispose]: () => {
+				this.unlock(path);
+			},
+		};
+		this.locks.set(path, lock);
+		return lock;
+	}
+
+	/**
+	 * Locks `path` asynchronously.
+	 * If the path is currently locked, waits for it to be unlocked.
+	 */
+	protected async lock(path: string): Promise<MutexLock> {
+		if (this.locks.has(path)) {
+			// Non-null assertion: we already checked locks has path
+			await this.locks.get(path)!.promise;
+		}
+
+		return this.addLock(path);
+	}
+
+	/**
+	 * Unlocks a path
+	 * @param path The path to lock
+	 * @param noThrow If true, an error will not be thrown if the path is already unlocked
+	 * @returns Whether the path was unlocked
+	 */
+	protected unlock(path: string, noThrow: boolean = false): boolean {
+		if (!this.locks.has(path)) {
+			if (noThrow) {
+				return false;
+			}
+			throw new ErrnoError(Errno.EPERM, 'Can not unlock an already unlocked path', path);
+		}
+
+		// Non-null assertion: we already checked locks has path
+		this.locks.get(path)!.resolve();
+		this.locks.delete(path);
+		return true;
+	}
+
+	/**
+	 * Attempt to lock `path` synchronously
+	 */
+	protected tryLock(path: string): boolean {
+		if (this.locks.has(path)) {
+			return false;
+		}
+
+		this.addLock(path);
+		return true;
+	}
+
+	protected isLocked(path: string): boolean {
+		return this.locks.has(path);
+	}
 
 	public async ready(): Promise<void> {
 		await this.fs.ready();
@@ -32,181 +98,148 @@ export class LockedFS<FS extends FileSystem> implements FileSystem {
 	}
 
 	public async rename(oldPath: string, newPath: string, cred: Cred): Promise<void> {
-		try {
-			await this.mutex.lock(oldPath);
-			await this.fs.rename(oldPath, newPath, cred);
-		} finally {
-			this.mutex.unlock(oldPath);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(oldPath);
+		await this.fs.rename(oldPath, newPath, cred);
 	}
 
 	public renameSync(oldPath: string, newPath: string, cred: Cred): void {
-		if (this.mutex.isLocked(oldPath)) {
+		if (this.isLocked(oldPath)) {
 			throw ErrnoError.With('EBUSY', oldPath, 'rename');
 		}
 		return this.fs.renameSync(oldPath, newPath, cred);
 	}
 
 	public async stat(path: string, cred: Cred): Promise<Stats> {
-		try {
-			await this.mutex.lock(path);
-			const stats = await this.fs.stat(path, cred);
-			return stats;
-		} finally {
-			this.mutex.unlock(path);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(path);
+		const stats = await this.fs.stat(path, cred);
+		return stats;
 	}
 
 	public statSync(path: string, cred: Cred): Stats {
-		if (this.mutex.isLocked(path)) {
+		if (this.isLocked(path)) {
 			throw ErrnoError.With('EBUSY', path, 'stat');
 		}
 		return this.fs.statSync(path, cred);
 	}
 
 	public async openFile(path: string, flag: string, cred: Cred): Promise<File> {
-		try {
-			await this.mutex.lock(path);
-			const fd = await this.fs.openFile(path, flag, cred);
-			return fd;
-		} finally {
-			this.mutex.unlock(path);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(path);
+		const fd = await this.fs.openFile(path, flag, cred);
+		return fd;
 	}
 
 	public openFileSync(path: string, flag: string, cred: Cred): File {
-		if (this.mutex.isLocked(path)) {
+		if (this.isLocked(path)) {
 			throw ErrnoError.With('EBUSY', path, 'openFile');
 		}
 		return this.fs.openFileSync(path, flag, cred);
 	}
 
 	public async createFile(path: string, flag: string, mode: number, cred: Cred): Promise<File> {
-		try {
-			await this.mutex.lock(path);
-			const fd = await this.fs.createFile(path, flag, mode, cred);
-			return fd;
-		} finally {
-			this.mutex.unlock(path);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(path);
+		const fd = await this.fs.createFile(path, flag, mode, cred);
+		return fd;
 	}
 
 	public createFileSync(path: string, flag: string, mode: number, cred: Cred): File {
-		if (this.mutex.isLocked(path)) {
+		if (this.isLocked(path)) {
 			throw ErrnoError.With('EBUSY', path, 'createFile');
 		}
 		return this.fs.createFileSync(path, flag, mode, cred);
 	}
 
 	public async unlink(path: string, cred: Cred): Promise<void> {
-		try {
-			await this.mutex.lock(path);
-			await this.fs.unlink(path, cred);
-		} finally {
-			this.mutex.unlock(path);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(path);
+		await this.fs.unlink(path, cred);
 	}
 
 	public unlinkSync(path: string, cred: Cred): void {
-		if (this.mutex.isLocked(path)) {
+		if (this.isLocked(path)) {
 			throw ErrnoError.With('EBUSY', path, 'unlink');
 		}
 		return this.fs.unlinkSync(path, cred);
 	}
 
 	public async rmdir(path: string, cred: Cred): Promise<void> {
-		try {
-			await this.mutex.lock(path);
-			await this.fs.rmdir(path, cred);
-		} finally {
-			this.mutex.unlock(path);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(path);
+		await this.fs.rmdir(path, cred);
 	}
 
 	public rmdirSync(path: string, cred: Cred): void {
-		if (this.mutex.isLocked(path)) {
+		if (this.isLocked(path)) {
 			throw ErrnoError.With('EBUSY', path, 'rmdir');
 		}
 		return this.fs.rmdirSync(path, cred);
 	}
 
 	public async mkdir(path: string, mode: number, cred: Cred): Promise<void> {
-		try {
-			await this.mutex.lock(path);
-			await this.fs.mkdir(path, mode, cred);
-		} finally {
-			this.mutex.unlock(path);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(path);
+		await this.fs.mkdir(path, mode, cred);
 	}
 
 	public mkdirSync(path: string, mode: number, cred: Cred): void {
-		if (this.mutex.isLocked(path)) {
+		if (this.isLocked(path)) {
 			throw ErrnoError.With('EBUSY', path, 'mkdir');
 		}
 		return this.fs.mkdirSync(path, mode, cred);
 	}
 
 	public async readdir(path: string, cred: Cred): Promise<string[]> {
-		try {
-			await this.mutex.lock(path);
-			const files = await this.fs.readdir(path, cred);
-			return files;
-		} finally {
-			this.mutex.unlock(path);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(path);
+		const files = await this.fs.readdir(path, cred);
+		return files;
 	}
 
 	public readdirSync(path: string, cred: Cred): string[] {
-		if (this.mutex.isLocked(path)) {
+		if (this.isLocked(path)) {
 			throw ErrnoError.With('EBUSY', path, 'readdir');
 		}
 		return this.fs.readdirSync(path, cred);
 	}
 
 	public async exists(path: string, cred: Cred): Promise<boolean> {
-		try {
-			await this.mutex.lock(path);
-			const exists = await this.fs.exists(path, cred);
-			return exists;
-		} finally {
-			this.mutex.unlock(path);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(path);
+		const exists = await this.fs.exists(path, cred);
+		return exists;
 	}
 
 	public existsSync(path: string, cred: Cred): boolean {
-		if (this.mutex.isLocked(path)) {
+		if (this.isLocked(path)) {
 			throw ErrnoError.With('EBUSY', path, 'exists');
 		}
 		return this.fs.existsSync(path, cred);
 	}
 
 	public async link(srcpath: string, dstpath: string, cred: Cred): Promise<void> {
-		try {
-			await this.mutex.lock(srcpath);
-			await this.fs.link(srcpath, dstpath, cred);
-		} finally {
-			this.mutex.unlock(srcpath);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(srcpath);
+		await this.fs.link(srcpath, dstpath, cred);
 	}
 
 	public linkSync(srcpath: string, dstpath: string, cred: Cred): void {
-		if (this.mutex.isLocked(srcpath)) {
+		if (this.isLocked(srcpath)) {
 			throw ErrnoError.With('EBUSY', srcpath, 'link');
 		}
 		return this.fs.linkSync(srcpath, dstpath, cred);
 	}
 
 	public async sync(path: string, data: Uint8Array, stats: Readonly<Stats>): Promise<void> {
-		try {
-			await this.mutex.lock(path);
-			await this.fs.sync(path, data, stats);
-		} finally {
-			this.mutex.unlock(path);
-		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		using _ = await this.lock(path);
+		await this.fs.sync(path, data, stats);
 	}
 
 	public syncSync(path: string, data: Uint8Array, stats: Readonly<Stats>): void {
-		if (this.mutex.isLocked(path)) {
+		if (this.isLocked(path)) {
 			throw ErrnoError.With('EBUSY', path, 'sync');
 		}
 		return this.fs.syncSync(path, data, stats);
