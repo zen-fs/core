6a9042ef5852174cfc1255c9fd50c2ed7c1ed885
James P. <jp@drvortex.dev>
2024-06-28 10:02:50 -0500

    Inlined exists variable

--- src/backends/overlay.ts
+++ src/backends/overlay.ts
@@ -86,8 +86,7 @@ export class UnlockedOverlayFS extends FileSystem {
 	public async sync(path: string, data: Uint8Array, stats: Readonly<Stats>): Promise<void> {
 		const cred = stats.cred(0, 0);
 		await this.createParentDirectories(path, cred);
-		const fileExists = await this._writable.exists(path, cred);
-		if (!fileExists) {
+		if (!await this._writable.exists(path, cred)) {
 			await this._writable.createFile(path, 'w', 0o644, cred);
 		}
 		await this._writable.sync(path, data, stats);
