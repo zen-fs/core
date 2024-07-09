5e5953eaf74907f1af188be500eb80a6f5708515
James P. <jp@drvortex.dev>
2024-07-08 20:04:31 -0500

    Seperated `configuration` modes

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,3 @@
-import { type Entries } from 'utilium';
 import type { Backend, BackendConfiguration, FilesystemOf, SharedConfig } from './backends/backend.js';
 import { checkOptions, isBackend, isBackendConfig } from './backends/backend.js';
 import * as fs from './emulation/index.js';
@@ -91,26 +90,23 @@ export interface Configuration<T extends ConfigMounts> extends SharedConfig {
 /**
  * Configures ZenFS with single mount point /
  */
-export async function configure<T extends Backend>(config: MountConfiguration<T>): Promise<void>;
+export async function configureSingle<T extends Backend>(config: MountConfiguration<T>): Promise<void> {
+	if (!isBackendConfig(config)) {
+		throw new TypeError('Invalid single mount point configuration');
+	}
+
+	const resolved = await resolveMountConfig(config);
+	fs.umount('/');
+	fs.mount('/', resolved);
+}
 
 /**
  * Configures ZenFS with the given configuration
  * @see Configuration
  */
-export async function configure<T extends ConfigMounts>(config: Partial<Configuration<T>>): Promise<void>;
-
-/**
- * Configures ZenFS with the given configuration
- * @see Configuration
- */
-export async function configure(config: MountConfiguration<Backend> | Partial<Configuration<ConfigMounts>>): Promise<void> {
+export async function configure<T extends ConfigMounts>(config: Partial<Configuration<T>>): Promise<void> {
 	const uid = 'uid' in config ? config.uid || 0 : 0;
 	const gid = 'gid' in config ? config.gid || 0 : 0;
-
-	if (isMountConfig(config)) {
-		// single FS
-		config = { mounts: { '/': config } } as Partial<Configuration<ConfigMounts>>;
-	}
 
 	setCred({ uid, gid, suid: uid, sgid: gid, euid: uid, egid: gid });
 
@@ -118,7 +114,7 @@ export async function configure(config: MountConfiguration<Backend> | Partial<Co
 		return;
 	}
 
-	for (const [point, mountConfig] of Object.entries(config.mounts) as Entries<typeof config.mounts>) {
+	for (const [point, mountConfig] of Object.entries(config.mounts)) {
 		if (!point.startsWith('/')) {
 			throw new ErrnoError(Errno.EINVAL, 'Mount points must have absolute paths');
 		}
@@ -127,7 +123,7 @@ export async function configure(config: MountConfiguration<Backend> | Partial<Co
 			mountConfig.disableAsyncCache ??= config.disableAsyncCache || false;
 		}
 
-		config.mounts[point] = await resolveMountConfig(mountConfig);
+		config.mounts[point as keyof T & `/${string}`] = await resolveMountConfig(mountConfig);
 	}
 
 	fs.mountObject(config.mounts as MountObject);
