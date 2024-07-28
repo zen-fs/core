46bc36da88b7476c0ec7985561562af633f2a5c4
uncore <contactuncor3@gmail.com>
2024-07-28 13:52:36 +0300

    add test for resolveRemoteMount

---
+++ tests/port/remote.browser.test.ts
@@ -0,0 +1,40 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { Worker } from 'node:worker_threads';
+import { Port } from '../../src/backends/port/fs.js';
+import { configureSingle, fs } from '../../src/index.js';
+
+const dir = dirname(fileURLToPath(import.meta.url));
+
+let port: Worker;
+
+try {
+	port = new Worker(dir + '/worker.browser.js');
+} catch (e) {
+	/* nothing */
+}
+
+describe('Remote FS with resolveRemoteMount', () => {
+	const content = 'FS is in a port';
+
+	test('Build exists for worker', () => {
+		expect(port).toBeDefined();
+	});
+
+	(port ? test : test.skip)('Configuration', async () => {
+		await configureSingle({ backend: Port, port, timeout: 300 });
+	});
+
+	(port ? test : test.skip)('Write', async () => {
+		await fs.promises.writeFile('/test', content);
+	});
+
+	(port ? test : test.skip)('Read', async () => {
+		expect(await fs.promises.readFile('/test', 'utf8')).toBe(content);
+	});
+
+	(port ? test : test.skip)('Cleanup', async () => {
+		await port.terminate();
+		port.unref();
+	});
+});
