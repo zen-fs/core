723bf25a585e52fe2884a46024cd54e50f37a7cb
James Prevett <jp@jamespre.dev>
2024-08-01 17:02:34 -0500

    Added shared eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,24 @@
 import tseslint from 'typescript-eslint';
-import globals from 'globals';
-import eslint from '@eslint/js';
+import shared from './eslint.shared.js';
 
 export default tseslint.config(
-	eslint.configs.recommended,
-	...tseslint.configs.recommended,
+	shared,
 	{
-		files: ['src/**/*', 'tests/**/*'],
+		files: ['src/**/*.ts', 'tests/**/*.ts'],
+		name: 'Enable typed checking',
 		languageOptions: {
-			globals: { ...globals.browser, ...globals.node },
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-		},
-		rules: {
-			'no-useless-escape': 'warn',
-			'no-unused-vars': 'off',
-			'no-mixed-spaces-and-tabs': 'warn',
-			'no-unreachable': 'warn',
-			'no-extra-semi': 'warn',
-			'no-fallthrough': 'off',
-			'no-empty': 'warn',
-			'no-case-declarations': 'off',
-			'prefer-const': 'warn',
-			'prefer-rest-params': 'warn',
-			'prefer-spread': 'warn',
-			'@typescript-eslint/no-unused-vars': 'warn',
-			'@typescript-eslint/no-inferrable-types': 'off',
-			'@typescript-eslint/no-this-alias': 'off',
-			//'@typescript-eslint/ban-types': 'warn',
-			'@typescript-eslint/triple-slash-reference': 'warn',
-			'@typescript-eslint/no-non-null-assertion': 'off',
-			'@typescript-eslint/no-namespace': 'warn',
-			'@typescript-eslint/prefer-as-const': 'warn',
-			'@typescript-eslint/no-explicit-any': 'warn',
-			'@typescript-eslint/consistent-type-assertions': 'warn',
-			'@typescript-eslint/consistent-type-imports': 'warn',
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
 		},
 	},
 	{
+		name: 'Allow explicit any in primary emulation layer files',
 		files: ['src/emulation/{sync,async,promises}.ts'],
 		rules: {
 			'@typescript-eslint/no-explicit-any': 'off',
 		},
 	},
-	{ ignores: ['tests/fixtures'] }
+	{ name: 'Ignore test fixtures', ignores: ['tests/fixtures'] }
 );
