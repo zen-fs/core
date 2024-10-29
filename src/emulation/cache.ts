f2fbb4d65f7ab5bbe8b4f38fd73e5929248d7e55
James Prevett <jp@jamespre.dev>
2024-10-29 11:45:23 -0500

    Added documentation to cache

--- src/emulation/cache.ts
+++ src/emulation/cache.ts
@@ -2,26 +2,41 @@
 
 import type { Stats } from '../stats.js';
 
+/**
+ * Whether the cache is enabled
+ */
 export let isEnabled = false;
 
+/**
+ * Sets whether the cache is enabled or not
+ */
 export function setEnabled(value: boolean): void {
 	isEnabled = value;
 }
 
 const stats = new Map<string, Stats>();
 
+/**
+ * Gets stats from the cache, if they exist and the cache is enabled.
+ */
 export function getStats(path: string): Stats | undefined {
 	if (!isEnabled) return;
 
 	return stats.get(path);
 }
 
+/**
+ * Adds stats if the cache is enabled
+ */
 export function setStats(path: string, value: Stats): void {
 	if (!isEnabled) return;
 
 	stats.set(path, value);
 }
 
+/**
+ * Clears the cache if it is enabled
+ */
 export function clearStats(): void {
 	if (!isEnabled) return;
 
