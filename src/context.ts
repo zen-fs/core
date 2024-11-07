ea0cf86da3a9fe102e8639c851a691c3144458ff
James Prevett <jp@jamespre.dev>
2024-11-07 15:10:43 -0600

    Added context

---
+++ src/context.ts
@@ -0,0 +1,33 @@
+import { getByString, type ExtractProperties } from 'utilium';
+import * as fs from './emulation/index.js';
+import type { AbsolutePath } from './emulation/path.js';
+import { credentials, type Credentials } from './credentials.js';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+type Fn_FS = keyof ExtractProperties<typeof fs, (...args: any[]) => any>;
+type Fn_Promises = keyof ExtractProperties<typeof fs.promises, (...args: any[]) => any>;
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
+type FnName = Fn_FS | `promises.${Fn_Promises}`;
+type Fn<T extends FnName> = T extends `promises.${infer U extends Fn_Promises}` ? (typeof fs.promises)[U] : T extends Fn_FS ? (typeof fs)[T] : never;
+
+/**
+ * Allows you to restrict operations to a specific root path and set of credentials.
+ * Not implemented.
+ * @experimental
+ */
+export class FSContext {
+	public constructor(
+		public readonly root: AbsolutePath,
+		public readonly creds: Credentials = credentials
+	) {}
+
+	public call<const K extends FnName>(method: K, ...args: Parameters<Fn<K>>): ReturnType<Fn<K>> {
+		// @ts-expect-error 2349
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		const value = getByString(fs, method)(...args);
+
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+		return value;
+	}
+}
