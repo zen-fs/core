0a4a3a9fe4c6c252875b4deacff603e1e52d4aa2
James Prevett <jp@jamespre.dev>
2024-11-07 19:18:43 -0600

    Update tests

--- tests/fs/times.test.ts
+++ tests/fs/times.test.ts
@@ -5,14 +5,14 @@ import { ErrnoError } from '../../dist/error.js';
 import { _toUnixTimestamp } from '../../dist/utils.js';
 import { fs } from '../common.js';
 
-suite('times', () => {
-	const path = 'x.txt';
+const path = 'x.txt';
 
+suite('times', () => {
 	function expect_assert(resource: string | number, atime: Date | number, mtime: Date | number) {
 		const stats = typeof resource == 'string' ? fs.statSync(resource) : fs.fstatSync(resource);
 		// check up to single-second precision since sub-second precision is OS and fs dependent
-		assert(_toUnixTimestamp(atime) == _toUnixTimestamp(stats.atime));
-		assert(_toUnixTimestamp(mtime) == _toUnixTimestamp(stats.mtime));
+		assert.equal(_toUnixTimestamp(atime), _toUnixTimestamp(stats.atime));
+		assert.equal(_toUnixTimestamp(mtime), _toUnixTimestamp(stats.mtime));
 	}
 
 	async function runTest(atime: Date | number, mtime: Date | number): Promise<void> {
@@ -24,24 +24,13 @@ suite('times', () => {
 			assert.strictEqual(error.code, 'ENOENT');
 		});
 
-		// don't close this fd
-		const handle = await fs.promises.open(path, 'r');
+		await using handle = await fs.promises.open(path, 'r');
 
 		await handle.utimes(atime, mtime);
 		expect_assert(handle.fd, atime, mtime);
 
 		fs.utimesSync(path, atime, mtime);
 		expect_assert(path, atime, mtime);
-
-		// some systems don't have futimes
-		// if there's an error, it be ENOSYS
-		try {
-			fs.futimesSync(handle.fd, atime, mtime);
-			expect_assert(handle.fd, atime, mtime);
-		} catch (error: any) {
-			assert(error instanceof ErrnoError);
-			assert.strictEqual(error.code, 'ENOSYS');
-		}
 
 		try {
 			fs.utimesSync('foobarbaz', atime, mtime);
@@ -59,11 +48,11 @@ suite('times', () => {
 	}
 
 	test('utimes works', async () => {
-		await runTest(new Date('1982/09/10 13:37:00'), new Date('1982/09/10 13:37:00'));
-		await runTest(new Date(), new Date());
-		await runTest(123456.789, 123456.789);
+		await test('new Date(...)', () => runTest(new Date('1982/09/10 13:37:00'), new Date('1982/09/10 13:37:00')));
+		await test('new Date()', () => runTest(new Date(), new Date()));
+		await test('number', () => runTest(123456.789, 123456.789));
 		const stats = fs.statSync(path);
-		await runTest(stats.atime, stats.mtime);
+		await test('from stats', () => runTest(stats.atime, stats.mtime));
 	});
 
 	test('read changes atime', async () => {
