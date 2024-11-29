eaa61af4fbeeb082c7c25eaf4c5546833c221ed1
James Prevett <jp@jamespre.dev>
2024-11-29 10:12:29 -0600

    Added `_` prefix to `isParentOf`

--- src/emulation/shared.ts
+++ src/emulation/shared.ts
@@ -103,7 +103,7 @@ export function resolveMount(path: string, ctx: V_Context): ResolvedMount {
 	const sortedMounts = [...mounts].sort((a, b) => (a[0].length > b[0].length ? -1 : 1)); // descending order of the string length
 	for (const [mountPoint, fs] of sortedMounts) {
 		// We know path is normalized, so it would be a substring of the mount point.
-		if (isParentOf(mountPoint, path)) {
+		if (_isParentOf(mountPoint, path)) {
 			path = path.slice(mountPoint.length > 1 ? mountPoint.length : 0); // Resolve the path relative to the mount point
 			if (path === '') {
 				path = root;
@@ -223,7 +223,7 @@ export function chroot<T extends V_Context>(this: T & V_Context, path: string, i
 /**
  * @internal @hidden
  */
-export function isParentOf(parent: string, child: string): boolean {
+export function _isParentOf(parent: string, child: string): boolean {
 	if (parent === '/' || parent === child) return true;
 
 	if (!parent.endsWith('/')) parent += '/';
