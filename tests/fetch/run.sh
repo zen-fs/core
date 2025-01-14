8602dfb21a9e118498d76d73e50ff04d03848f20
James Prevett <jp@jamespre.dev>
2025-01-13 19:42:49 -0600

    Fix `tsx` no having `npx` prefix

--- tests/fetch/run.sh
+++ tests/fetch/run.sh
@@ -3,7 +3,7 @@
 # Credit: Dave Dopson, https://stackoverflow.com/a/246128/17637456
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-tsx $SCRIPT_DIR/server.ts &
+npx tsx $SCRIPT_DIR/server.ts &
 PID=$!
 
 npx zenfs-test $SCRIPT_DIR/setup.ts --preserve --force $@
