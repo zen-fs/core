3b112351be1c3164cb99c7823ffbe1734a58e155
James Prevett <jp@jamespre.dev>
2025-02-12 19:45:37 -0600

    Polyfills now log warnings and aren't considered in test coverage

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,21 +1,28 @@
+/* node:coverage disable */
+
+import { warn } from './internal/log.js';
+
 // eslint-disable-next-line @typescript-eslint/unbound-method
-Promise.withResolvers ??= function <T>(): PromiseWithResolvers<T> {
-	let _resolve: ((value: T | PromiseLike<T>) => void) | undefined,
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		_reject: ((reason?: any) => void) | undefined;
-	const promise = new Promise<T>((resolve, reject) => {
-		_resolve = resolve;
-		_reject = reject;
+Promise.withResolvers ??=
+	(warn('Using a polyfill of Promise.withResolvers'),
+	function <T>(): PromiseWithResolvers<T> {
+		let _resolve: ((value: T | PromiseLike<T>) => void) | undefined,
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			_reject: ((reason?: any) => void) | undefined;
+		const promise = new Promise<T>((resolve, reject) => {
+			_resolve = resolve;
+			_reject = reject;
+		});
+		return { promise, resolve: _resolve!, reject: _reject! };
 	});
-	return { promise, resolve: _resolve!, reject: _reject! };
-};
 
 // @ts-expect-error 2540
-Symbol['dispose'] ??= Symbol('Symbol.dispose');
+Symbol['dispose'] ??= (warn('Using a polyfill of Symbol.dispose'), Symbol('Symbol.dispose'));
 // @ts-expect-error 2540
-Symbol['asyncDispose'] ??= Symbol('Symbol.asyncDispose');
+Symbol['asyncDispose'] ??= (warn('Using a polyfill of Symbol.asyncDispose'), Symbol('Symbol.asyncDispose'));
 
 declare global {
 	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 	interface AsyncIterator<T, TReturn, TNext> extends AsyncDisposable {}
 }
+/* node:coverage enable */
