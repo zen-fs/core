15c03783645a09b948e63e58a004a881040d2758
James Prevett <jp@jamespre.dev>
2025-03-09 16:42:09 -0500

    Exported `ioctl`

--- src/vfs/index.ts
+++ src/vfs/index.ts
@@ -1,9 +1,10 @@
-export { BigIntStatsFs, Stats, StatsFs } from './stats.js';
 export * from './async.js';
 export * as constants from './constants.js';
 export * from './dir.js';
+export * from './ioctl.js';
 export * as promises from './promises.js';
-export * as xattr from './xattr.js';
 export { chroot, mount, umount } from './shared.js';
+export { BigIntStatsFs, Stats, StatsFs } from './stats.js';
 export * from './streams.js';
 export * from './sync.js';
+export * as xattr from './xattr.js';
