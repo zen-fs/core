43e391819bd84c595e2119f67766fbbc18492032
Sean Whiteman <seanw265@gmail.com>
2025-03-19 11:35:59 -0400

    tests(SingleBuffer): Implement test for SingleBuffer with SharedArrayBuffer in Node environment

--- tests/backend/single-buffer.test.ts
+++ tests/backend/single-buffer.test.ts
@@ -1,6 +1,7 @@
 import { test, suite } from 'node:test';
 import { fs, mount, resolveMountConfig, SingleBuffer, umount } from '../../dist/index.js';
 import assert from 'node:assert';
+import { Worker } from 'worker_threads';
 
 await suite('SingleBuffer', () => {
 	test('should be able to restore filesystem (with same metadata) from original buffer', async () => {
@@ -20,5 +21,28 @@ await suite('SingleBuffer', () => {
 		const snapshotStats = fs.statSync('/example.ts');
 
 		assert.deepEqual(snapshotStats, stats);
+	});
+
+	test('should support SharedArrayBuffer across threads', async () => {
+		const sharedBuffer = new SharedArrayBuffer(0x100000);
+
+		umount('/');
+		const writable = await resolveMountConfig({ backend: SingleBuffer, buffer: sharedBuffer });
+		mount('/', writable);
+
+		const worker = new Worker(import.meta.dirname + '/single-buffer.worker.js', { workerData: sharedBuffer });
+
+		// Pause while we wait for the worker to emit the 'continue' message, which
+		// means it has mounted the filesystem and created /worker-file.ts
+		await new Promise<void>(resolve => {
+			worker.on('message', message => {
+				if (message === 'continue') resolve();
+			});
+		});
+
+		worker.terminate();
+		worker.unref();
+
+		assert(fs.existsSync('/worker-file.ts'));
 	});
 });
