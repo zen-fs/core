837bd249f7cf4810a110387a55703f1ff5f240a7
Jeff Hykin <jeff.hykin@gmail.com>
2025-04-20 14:07:42 -0500

    add example to bindContext

--- src/context.ts
+++ src/context.ts
@@ -21,6 +21,36 @@ export const boundContexts = new Map<number, BoundContext>();
  * Allows you to restrict operations to a specific root path and set of credentials.
  * Note that the default credentials of a bound context are copied from the global credentials.
  * @category Contexts
+ * @example
+ * ```ts
+ * import { bindContext, fs as root } from '@zenfs/core'; // or your existing
+ * import assert from 'node:assert';
+ * 
+ * for(const i of [1, 2, 3]) root.mkdirSync('/' + i);
+ * root.mkdirSync('/root_1');
+ * 
+ * const { fs: fs1 } = bindContext({ root: '/1' });
+ * const { fs: fs2 } = bindContext({ root: '/2' });
+ * const { fs: fs3 } = bindContext({ root: '/3' });
+ * const ctx = bindContext({ root: '/root_1' }); // note the PWD is relative to the context's root
+ * 
+ * ctx.fs.mkdirSync('/sub');
+ * const { fs: sub } = ctx.bind({ root: '/sub' });
+ * sub.writeFileSync('double-nested.txt', 'whoa');
+ * 
+ * assert(root.existsSync('/root_1/sub'));
+ * 
+ * fs1.writeFileSync('/example.txt', 'fs1');
+ * fs2.writeFileSync('/example.txt', 'fs2');
+ * fs3.writeFileSync('/example.txt', 'fs3');
+ * ctx.fs.writeFileSync('/example.txt', 'fs4');
+ * 
+ * assert(!root.existsSync('/example.txt'));
+ * 
+ * assert.equal(root.readFileSync('/root_1/example.txt', 'utf8'), 'fs4');
+ * 
+ * assert.deepEqual(ctx.fs.readdirSync('/../../../..'), root.readdirSync('/root_1'));
+ * ```
  */
 export function bindContext(
 	this: void | null | FSContext,
