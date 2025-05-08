4bcff464bea95a04da8406a8119e020dd0534e7c
James Prevett <jp@jamespre.dev>
2025-05-08 11:52:23 -0500

    `crypto.randomUUID` is now polyfilled when it is missing

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,8 +1,9 @@
 /* node:coverage disable */
+/* eslint-disable @typescript-eslint/unbound-method */
 
 import { warn } from 'kerium/log';
+import type { UUID } from 'node:crypto';
 
-// eslint-disable-next-line @typescript-eslint/unbound-method
 Promise.withResolvers ??=
 	(warn('Using a polyfill of Promise.withResolvers'),
 	function <T>(): PromiseWithResolvers<T> {
@@ -20,5 +21,15 @@ Promise.withResolvers ??=
 Symbol['dispose'] ??= (warn('Using a polyfill of Symbol.dispose'), Symbol('Symbol.dispose'));
 // @ts-expect-error 2540
 Symbol['asyncDispose'] ??= (warn('Using a polyfill of Symbol.asyncDispose'), Symbol('Symbol.asyncDispose'));
+
+function randomUUID(): UUID {
+	const bytes = crypto.getRandomValues(new Uint8Array(16));
+	bytes[6] = (bytes[6] & 0x0f) | 0x40;
+	bytes[8] = (bytes[8] & 0x3f) | 0x80;
+	const hex = [...bytes].map(b => b.toString(16).padStart(2, '0')).join('');
+	return `${hex.slice(0, 8)}-${hex.slice(8, 12)}-${hex.slice(12, 16)}-${hex.slice(16, 20)}-${hex.slice(20)}`;
+}
+
+globalThis.crypto.randomUUID ??= (warn('Using a polyfill of crypto.randomUUID'), randomUUID);
 
 /* node:coverage enable */
