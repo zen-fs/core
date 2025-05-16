615ef789934701d975af3976ab8cc474e69d7c72
James Prevett <jp@jamespre.dev>
2025-05-16 18:02:46 -0500

    Added polyfill for `Uint8Array` methods: to/from base64/hex

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -22,14 +22,47 @@ Symbol['dispose'] ??= (warn('Using a polyfill of Symbol.dispose'), Symbol('Symbo
 // @ts-expect-error 2540
 Symbol['asyncDispose'] ??= (warn('Using a polyfill of Symbol.asyncDispose'), Symbol('Symbol.asyncDispose'));
 
-function randomUUID(): UUID {
-	const bytes = crypto.getRandomValues(new Uint8Array(16));
-	bytes[6] = (bytes[6] & 0x0f) | 0x40;
-	bytes[8] = (bytes[8] & 0x3f) | 0x80;
-	const hex = [...bytes].map(b => b.toString(16).padStart(2, '0')).join('');
-	return `${hex.slice(0, 8)}-${hex.slice(8, 12)}-${hex.slice(12, 16)}-${hex.slice(16, 20)}-${hex.slice(20)}`;
-}
+globalThis.crypto.randomUUID ??=
+	(warn('Using a polyfill of crypto.randomUUID'),
+	function randomUUID(): UUID {
+		const bytes = crypto.getRandomValues(new Uint8Array(16));
+		bytes[6] = (bytes[6] & 0x0f) | 0x40;
+		bytes[8] = (bytes[8] & 0x3f) | 0x80;
+		const hex = [...bytes].map(b => b.toString(16).padStart(2, '0')).join('');
+		return `${hex.slice(0, 8)}-${hex.slice(8, 12)}-${hex.slice(12, 16)}-${hex.slice(16, 20)}-${hex.slice(20)}`;
+	});
 
-globalThis.crypto.randomUUID ??= (warn('Using a polyfill of crypto.randomUUID'), randomUUID);
+Uint8Array.prototype.toBase64 ??=
+	(warn('Using a polyfill of Uint8Array.prototype.toBase64'),
+	function toBase64(this: Uint8Array): string {
+		return btoa(String.fromCharCode(...this));
+	});
+
+Uint8Array.fromBase64 ??=
+	(warn('Using a polyfill of Uint8Array.fromBase64'),
+	function fromBase64(this: Uint8Array, base64: string): Uint8Array {
+		const binaryString = atob(base64);
+		const bytes = new Uint8Array(binaryString.length);
+		for (let i = 0; i < binaryString.length; i++) {
+			bytes[i] = binaryString.charCodeAt(i);
+		}
+		return bytes;
+	});
+
+Uint8Array.prototype.toHex ??=
+	(warn('Using a polyfill of Uint8Array.prototype.toHex'),
+	function toHex(this: Uint8Array): string {
+		return [...this].map(b => b.toString(16).padStart(2, '0')).join('');
+	});
+
+Uint8Array.fromHex ??=
+	(warn('Using a polyfill of Uint8Array.fromHex'),
+	function fromHex(this: Uint8Array, hex: string): Uint8Array {
+		const bytes = new Uint8Array(hex.length / 2);
+		for (let i = 0; i < hex.length; i += 2) {
+			bytes[i / 2] = parseInt(hex.slice(i, i + 2), 16);
+		}
+		return bytes;
+	});
 
 /* node:coverage enable */
