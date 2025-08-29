5a5e83f803192f9f7e61530aeb09413e11c01fe1
James Prevett <jp@jamespre.dev>
2025-08-29 18:53:55 -0500

    Unhandled rejections in tests are now logged

--- tests/common.ts
+++ tests/common.ts
@@ -1,11 +1,16 @@
 import { join, resolve } from 'node:path';
 import { fs as defaultFS } from '../dist/index.js';
 import { setupLogs } from './logs.js';
+import { styleText } from 'node:util';
 export type * from '../dist/index.js';
 
 setupLogs();
 
 const setupPath = resolve(process.env.SETUP || join(import.meta.dirname, 'setup/memory.ts'));
+
+process.on('unhandledRejection', (reason: Error) => {
+	console.error('Unhandled rejection:', styleText('red', reason.stack || reason.message));
+});
 
 const setup = await import(setupPath).catch(error => {
 	console.log('Failed to import test setup:');
