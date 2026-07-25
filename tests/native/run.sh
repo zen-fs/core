047000ca1836dc36e9f9ff8186d23a439e7d686f
James Prevett <jp@jamespre.dev>
2026-07-25 11:11:13 -0500

    Test native errors as well

--- tests/native/run.sh
+++ tests/native/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo npx zenfs-test -v tests/native/setup.ts -s 'Extended Attributes' -s 'Error messages'
+sudo npx zenfs-test -v tests/native/setup.ts -s 'Extended Attributes'
