1bce8370b568868d4af675ef46412a8b2be105cd
James Prevett <jp@jamespre.dev>
2026-09-11 18:40:38 -0500

    Removed unused eslint disable rule

--- src/node/streams.ts
+++ src/node/streams.ts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-/* eslint-disable @typescript-eslint/triple-slash-reference, @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import type { Abortable } from 'node:events';
 import type * as fs from 'node:fs';
 import type { CreateReadStreamOptions, CreateWriteStreamOptions } from 'node:fs/promises';
